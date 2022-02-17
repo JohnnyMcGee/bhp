975caf8500463f23214ac7892715cb07753b26ab
Johnny McGee <johnnymcgee.dev@protonmail.com>
2022-02-17 07:48:33 -0500

    Start implementing NetCat class

--- ch1/netcat.py
+++ ch1/netcat.py
@@ -16,6 +16,39 @@ def execute(cmd):
         stderr=subprocess.STDOUT)
     return output.decode()
 
+class NetCat:
+    def __init__(self, args, buffer=None):
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+    
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffe)
+        try:
+            while True:
+                recv_len = 1
+                response = ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
+                        break
+                    if response:
+                        print(response)
+                        buffer = input('> ')
+                        buffer += '\n'
+                    self.socket.send(buffer.encode())
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='BHP Net Tool',
