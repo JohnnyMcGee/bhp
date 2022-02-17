0740915af5aac658e67d20847bd9713e72fabe28
Johnny McGee <johnnymcgee.dev@protonmail.com>
2022-02-17 07:40:04 -0500

    Add argument parser with help epilog, arguments

--- ch1/netcat.py
+++ ch1/netcat.py
@@ -10,7 +10,7 @@ import threading
 def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
-        return 1
+        return
     output = subprocess.check_output(
         shlex.split(cmd),
         stderr=subprocess.STDOUT)
@@ -33,3 +33,16 @@ if __name__ == '__main__':
         # connect to server
         '''))
     parser.add_argument('-c', '--command', action='store_true', help='command shell')
+    parser.add_argument('-e', '--execute', help='execute specified command')
+    parser.add_argument('-l', '--listen', action='store_true', help='listen')
+    parser.add_argument('p', '--port', type=int, default=5555, help='specified port')
+    parser.add_argument('t', '--target', default='192.168.1.203', help='specified IP')
+
+    args = parser.parse_args()
+    if args.listen:
+        buffer = ''
+    else:
+        buffer = sys.stdin.read()
+    nc = NetCat(args, buffer.encode())
+    nc.run()
+
