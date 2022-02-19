553ee702ec091bc12a28599270a8e35f0acc6c62
Johnny McGee <johnnymcgee.dev@protonmail.com>
2022-02-19 18:51:09 -0500

    Add listen and handle methods to NetCat class

--- ch1/netcat.py
+++ ch1/netcat.py
@@ -1,5 +1,5 @@
 import argparse
-import imp
+# import imp
 import socket
 import shlex
 import subprocess
@@ -32,7 +32,7 @@ class NetCat:
     def send(self):
         self.socket.connect((self.args.target, self.args.port))
         if self.buffer:
-            self.socket.send(self.buffe)
+            self.socket.send(self.buffer)
         try:
             while True:
                 recv_len = 1
@@ -48,6 +48,53 @@ class NetCat:
                         buffer = input('> ')
                         buffer += '\n'
                     self.socket.send(buffer.encode())
+        except KeyboardInterrupt:
+            print('User terminated.')
+            self.socket.close()
+            sys.exit()
+    
+    def listen(self):
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+        while True:
+            client_socket, _ = self.socket.accept()
+            client_thread = threading.Thread(
+                target=self.handle,
+                args=(client_socket)
+            )
+            client_thread.start()
+    
+    def handle(self, client_socket):
+        if self.args.execute:
+            output = execute(self.args.execute)
+            client_socket.send(output.encode())
+        elif self.args.upload:
+            file_buffer = b''
+            while True:
+                data = client_socket.recv(4096)
+                if data:
+                    file_buffer += data
+                else:
+                    break
+            with open(self.args.upload, 'wb') as f:
+                f.write(file_buffer)
+            message = f'Saved file {self.args.upload}'
+            client_socket.send(message.encode())
+        elif self.args.command:
+            cmd_buffer = b''
+            while True:
+                try:
+                    client_socket.send(b'BHP: #> ')
+                    while '\n' not in cmd_buffer.decode():
+                        cmd_buffer += client_socket.recv(64)
+                    response = execute(cmd_buffer.decode())
+                    if response:
+                        client_socket.send(response.encode())
+                        cmd_buffer = b''
+                except Exception as e:
+                    print(f'server killed {e}')
+                    self.socket.close()
+                    sys.exit()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -68,8 +115,8 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--command', action='store_true', help='command shell')
     parser.add_argument('-e', '--execute', help='execute specified command')
     parser.add_argument('-l', '--listen', action='store_true', help='listen')
-    parser.add_argument('p', '--port', type=int, default=5555, help='specified port')
-    parser.add_argument('t', '--target', default='192.168.1.203', help='specified IP')
+    parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
+    parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
 
     args = parser.parse_args()
     if args.listen:
