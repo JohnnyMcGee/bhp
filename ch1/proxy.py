95c849c6cda001ce829f1bfa9f01510b71b9b459
Johnny McGee <johnnymcgee.dev@protonmail.com>
2022-02-21 20:44:28 -0500

    Add hexdump and receive_from functions to facilitate and display incoming data

--- ch1/proxy.py
+++ ch1/proxy.py
@@ -3,5 +3,35 @@ import socket
 import threading
 
 
+
 HEX_FILTER = ''.join([(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)])
 
+# Takes a string or bytestring, outputs hex and ascii
+def hexdump(src, length=16, show=True):
+    if isinstance(src, bytes):
+        src = src.decode()
+    results = list()
+    for i in range(0, len(src), length):
+        word = str(src[i:i+length])
+        printable = word.translate(HEX_FILTER)
+        hexa = ' '.join([f'{ord(c):02x}' for c in word])
+        hexwidth = length*3
+        results.append(f'{i:04x} {hexa:<{hexwidth}} {printable}')
+    if show:
+        for line in results:
+            print(line)
+    else:
+        return results
+
+def receive_from(connection, timeout=5):
+    buffer = b''
+    connection.settimeout(timeout)
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            buffer += data
+    except Exception as e:
+        pass
+    return buffer
