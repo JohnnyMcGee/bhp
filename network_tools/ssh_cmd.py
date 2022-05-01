e4ec584f373eeddbf267af8aca1e3ab42f82a53a
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-05-01 12:49:15 -0500

    Fix syntax and set default port to 22

--- network_tools/ssh_cmd.py
+++ network_tools/ssh_cmd.py
@@ -5,7 +5,7 @@ def ssh_command(ip, port, user, passwd, cmd):
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(ip, port=port, username=user, password=passwd)
     _, stdout, stderr = client.exec_command(cmd)
-    output = stdout.readlines() + stderr.readlines
+    output = stdout.readlines() + stderr.readlines()
 
     if output:
         print('--- Output ---')
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     user = input('Username: ')
     password = getpass.getpass()
     ip = input('Enter server IP: ') or '127.0.0.1'
-    port = input('Enter port or <CR>: ') or 2222
+    port = input('Enter port or <CR>: ') or 22
     cmd = input('Enter command or <CR>: ') or 'id'
 
     ssh_command(ip, port, user, password, cmd)
